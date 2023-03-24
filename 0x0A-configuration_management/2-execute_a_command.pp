4a32518054eaa2c149474054af86ac3af6ba390f
Willis <Williamstone4041@gmail.com>
2023-03-24 18:04:31 +0100

    final commit

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,7 @@
 # Using Puppet, create a manifest that kills a process named 'killmenow'
 # Must use 'exec' and 'pkill'
 
-exec { 'pkill killmenow':
-  command => '/usr/bin/pkill -f /killmenow',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
