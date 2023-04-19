14782232f86ebb2abc6935f426d1e77c2bd00588
WillisXoX <williamstone4041@gmail.com>
2023-04-19 07:12:26 +0000

    2-if_only_you_could_see.sql

--- 0x14-mysql/1.let_us_in.sql
+++
@@ -1,4 +0,0 @@
--- Create a MySQL user named holberton_user set to localhost and the password projectcorrection280hbtn with permission to check the primary/replica status of your databases
-
-GRANT REPLICATION CLIENT ON *.*
-TO 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
